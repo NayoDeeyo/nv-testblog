c5761391b69eb4af95967188849c38853a7f92b3
Nayo Deeyo <call0979758170@gmail.com>
2023-08-23 00:28:11 +0700

    'testmidtermexam'

--- server/src/app.js
+++ server/src/app.js
@@ -1,1 +1,26 @@
-console.log('Hello nodejs server')
+let express = require('express')
+const app = express()
+
+app.get('/status', function (req, res){
+    res.send('hekko nodejs server')
+})
+
+app.get('/hello/:person', function (req,res) {
+    console.log('hello -' + req.params.person)
+    res.send('say hello with ' + req.params.person)
+})
+
+//get user by id
+app.get('/user/:userId', function (req, res) {
+    res.send('ดูข้อมูลผู้ใช้งาน')
+})
+
+//get all user
+app.get('/users', function (req, res) {
+    res.send('เรียกดูข้อมูลผู้ใช้งานทั้งหมด')
+})
+
+let port = 8081
+app.listen(port, function () {
+    console.log('server running on' + port)
+})
