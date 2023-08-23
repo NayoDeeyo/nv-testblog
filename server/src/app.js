42bf1ffedbb68c24e854c41b0931f6bbce3978b5
Nayo Deeyo <call0979758170@gmail.com>
2023-08-23 10:25:27 +0700

    'TestMidtermExam'

--- server/src/app.js
+++ server/src/app.js
@@ -1,5 +1,10 @@
 let express = require('express')
+let bodyParser = require('body-parser')
+
 const app = express()
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
 
 app.get('/status', function (req, res){
     res.send('hekko nodejs server')
@@ -12,12 +17,27 @@ app.get('/hello/:person', function (req,res) {
 
 //get user by id
 app.get('/user/:userId', function (req, res) {
-    res.send('ดูข้อมูลผู้ใช้งาน')
+    res.send('ดูข้อมูลผู้ใช้งาน : ' + req.params.userId)
 })
 
 //get all user
 app.get('/users', function (req, res) {
     res.send('เรียกดูข้อมูลผู้ใช้งานทั้งหมด')
+})
+
+// create user
+app.post('/users/', function (req, res) {
+    res.send('ทำการสร้างผู้ใช้งาน' + JSON.stringify(req.body))
+})
+
+// edit user
+app.put('/user/:userId', function (req, res) {
+    res.send('ทำการแก้ไขผู้ใช้งาน' + req.params.userId + ' : ' + JSON.stringify(req.body))
+})
+
+// delete user
+app.delete('/user/:userId', function (req, res) {
+    res.send('ทำการลบผู้ใช้งาน' + req.params.userId + ' : ' + JSON.stringify(req.body))
 })
 
 let port = 8081
